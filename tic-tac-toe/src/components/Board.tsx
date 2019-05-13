78e5a6ff7b03df2c53cb5ec6249a48a9add359d1
Akira Kanno <teinen.github@gmail.com>
2019-05-14 07:40:12 +0900

    Add squares state to Board

--- tic-tac-toe/src/components/Board.tsx
+++ tic-tac-toe/src/components/Board.tsx
@@ -2,35 +2,50 @@ import React from 'react'
 import Square from './Square'
 import './Board.css'
 
-const Board: React.FC = () => {
-  const status = 'Next player: X'
+interface Props {}
 
-  const renderSquare = (i: number) => {
-    return <Square value={i} />
+interface State {
+  squares: Array<number>
+}
+
+class Board extends React.Component<Props, State> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null)
+    }
   }
 
-  return (
-    <div>
-      <div className="status">
-        { status }
+  status = 'Next player: X'
+
+  renderSquare(i: number) {
+    return <Square value={ this.state.squares[i] } />
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="status">
+          { this.status }
+        </div>
+        <div className="board-row">
+          { this.renderSquare(0) }
+          { this.renderSquare(1) }
+          { this.renderSquare(2) }
+        </div>
+        <div className="board-row">
+          { this.renderSquare(3) }
+          { this.renderSquare(4) }
+          { this.renderSquare(5) }
+        </div>
+        <div className="board-row">
+          { this.renderSquare(6) }
+          { this.renderSquare(7) }
+          { this.renderSquare(8) }
+        </div>
       </div>
-      <div className="board-row">
-        { renderSquare(0) }
-        { renderSquare(1) }
-        { renderSquare(2) }
-      </div>
-      <div className="board-row">
-        { renderSquare(3) }
-        { renderSquare(4) }
-        { renderSquare(5) }
-      </div>
-      <div className="board-row">
-        { renderSquare(6) }
-        { renderSquare(7) }
-        { renderSquare(8) }
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Board
