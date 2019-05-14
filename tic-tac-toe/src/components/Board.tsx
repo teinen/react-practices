cfa4cb94fe6a69a61153dd15041a949098536d79
Akira Kanno <teinen.github@gmail.com>
2019-05-14 23:12:57 +0900

    Add calculate winner function

--- tic-tac-toe/src/components/Board.tsx
+++ tic-tac-toe/src/components/Board.tsx
@@ -5,7 +5,7 @@ import './Board.css'
 interface Props {}
 
 interface State {
-  squares: Array<number | string>,
+  squares: string[],
   xIsNext: boolean
 }
 
@@ -20,6 +20,9 @@ class Board extends React.Component<Props, State> {
 
   clickHandler(i: number): void {
     const squares = this.state.squares.slice()
+    if (this.calculateWinner(squares) || squares[i]) {
+      return
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       squares: squares,
@@ -36,8 +39,29 @@ class Board extends React.Component<Props, State> {
     )
   }
 
+  calculateWinner(squares: string[]): string | undefined {
+    const lines = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+      [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6],
+    ]
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i]
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a]
+      }
+      return
+    }
+  }
+
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
+    const winner = this.calculateWinner(this.state.squares)
+    let status
+    if (winner) {
+      status = `Winner: ${winner}`
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
+    }
 
     return (
       <div>
