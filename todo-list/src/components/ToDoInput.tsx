1030e35d85317cd72aa514f219911b8adf8d4fa2
Akira Kanno <teinen.github@gmail.com>
2019-05-19 19:50:03 +0900

    Add input styles

--- todo-list/src/components/ToDoInput.tsx
+++ todo-list/src/components/ToDoInput.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useRef } from 'react'
+import '../style/ToDoInput.css'
 
 interface Props {
   onClick: (todo: string) => void
 }
 
 const ToDoInput: React.FC<Props> = (props) => {
-  const [newTodo, setNewTodo] = useState('')
   const inputRef = useRef({} as HTMLInputElement)
 
   const clickHandler = (newTodo: string): void => {
@@ -14,16 +14,19 @@ const ToDoInput: React.FC<Props> = (props) => {
   }
 
   return (
-    <div>
+    <div className="todo-input-container">
       <input
         ref={inputRef}
         type="text"
         placeholder="Input new todo"
-        onChange={(e) => { setNewTodo(e.target.value) }}
+        className="todo-input"
       >
 
       </input>
-      <button onClick={() => { clickHandler(newTodo) }}>
+      <button
+        className="todo-add-btn"
+        onClick={() => { clickHandler(inputRef.current.value) }}
+      >
         Add
       </button>
     </div>
