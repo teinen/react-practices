ccd075ceffe7d97c5e71fcda284604787974c0c3
Akira Kanno <teinen.github@gmail.com>
2019-05-19 18:06:33 +0900

    Initial commit of ToDoApp

---
+++ todo-list/src/components/ToDoApp.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../style/ToDoApp.css'
+
+const App: React.FC = () => {
+  return (
+    <div className="todo-app">
+      <div className="hoge"></div>
+    </div>
+  )
+}
+
+export default App;
